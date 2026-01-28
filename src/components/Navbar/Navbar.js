2ef9766af1af3d29a6704e30c7095b711b5c6a55
DharaGabani24 <dharagabani4440@gmail.com>
2026-01-28 17:22:26 +0530

    GOC svg

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,14 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="nav">
-      <a href="#home" className="nav__logo">CL</a>
+      {/* <a href="#home" className="nav__logo">GOC</a> */}
+      <a href="#home" className="nav__logo">
+        <img 
+          src={process.env.PUBLIC_URL + '/img/GOC_logo.webp'} 
+          alt="Gods of Cricket" 
+          className="nav__logo-img"
+        />
+      </a>
       <ul className="nav__menu">
         <li><a href="#features" className="nav__link">Features</a></li>
         <li><a href="#stadiums" className="nav__link">Stadiums</a></li>
